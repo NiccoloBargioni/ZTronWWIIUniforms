299a0868024951d8605c00f662b7ef020b68dc1a
NickTheFreak97 <niccolo.dellarocca@gmail.com>
2024-10-13 13:49:34 +0200

    Home Advantage keys fixed

--- Sources/ZTronWWIIUniforms/Model/Challenge, TaggedString.swift
+++ Sources/ZTronWWIIUniforms/Model/Challenge, TaggedString.swift
@@ -31,7 +31,7 @@ public class Challenge<Tag>: Hashable, Identifiable where Tag: Hashable {
     }
     
     public func getName() -> String {
-        return String(localized: String.LocalizationValue(stringLiteral: self.name), bundle: Bundle.module)
+        return String(localized: String.LocalizationValue(stringLiteral: self.name), bundle: .module)
     }
     
     public func getMap() -> String {
