357dfd7c59e545f80de7ed39df552124bd2a4663
= <niccolo.dellarocca@gmail.com>
2024-10-15 14:45:12 +0200

    Completed BAT Elite

--- Sources/ZTronWWIIUniforms/ChallengeRequirements/The Final Reich/Challenges/makeAttivitaSegreteChallenges.swift
+++ Sources/ZTronWWIIUniforms/ChallengeRequirements/The Final Reich/Challenges/makeAttivitaSegreteChallenges.swift
@@ -2,6 +2,43 @@ import Foundation
 import LoremIpsum
 
 internal func makeAttivitaSegreteChallenges() -> [Challenge<String>] {
+    let sharedHardcoreEasterEggProTips = [
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip1".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip1Tag".fromLocalized()
+        ),
+        
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip2".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip2Tag".fromLocalized()
+        ),
+        
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip3".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip3Tag".fromLocalized()
+        ),
+        
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip4".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip4Tag".fromLocalized()
+        ),
+        
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip5".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip5Tag".fromLocalized()
+        ),
+        
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip6".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip6Tag".fromLocalized()
+        ),
+        
+        Challenge<String>.TaggedString(
+            "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip7".fromLocalized(),
+            tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip7Tag".fromLocalized()
+        ),
+    ]
+    
     return [
         Challenge<String>(
             name: "OUTFIT.B.A.T._ELITE.OFF_RECORD.ONE_MAN_ARMY.ChallengeName".fromLocalized(),
@@ -43,6 +80,7 @@ internal func makeAttivitaSegreteChallenges() -> [Challenge<String>] {
             ]
         ),
         
+        
         Challenge<String>(
             name: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ChallengeName".fromLocalized(),
             map: "the final reich",
@@ -55,43 +93,86 @@ internal func makeAttivitaSegreteChallenges() -> [Challenge<String>] {
             ],
             donts: [],
             glitches: [],
+            proTips: sharedHardcoreEasterEggProTips
+        ),
+        
+        
+        Challenge<String>(
+            name: "OUTFIT.B.A.T._ELITE.OFF_RECORD.TIME_IS_OF_ESSENCE.ChallengeName".fromLocalized(),
+            map: "the final reich",
+            riddle: "OUTFIT.B.A.T._ELITE.OFF_RECORD.TIME_IS_OF_ESSENCE.Riddle".fromLocalized(),
+            requirements: [
+                Challenge<String>.TaggedString(
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.TIME_IS_OF_ESSENCE.Requirement1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.TIME_IS_OF_ESSENCE.Requirement1Tag".fromLocalized()
+                )
+            ],
+            donts: [],
+            glitches: [],
+            proTips: sharedHardcoreEasterEggProTips
+        ),
+        
+        
+        Challenge<String>(
+            name: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.ChallengeName".fromLocalized(),
+            map: "the final reich",
+            riddle: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.Riddle".fromLocalized(),
+            requirements: [
+                Challenge<String>.TaggedString(
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.Requirement1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.Requirement1Tag".fromLocalized()
+                )
+            ],
+            donts: [],
+            glitches: [],
             proTips: [
                 Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip1".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip1Tag".fromLocalized()
-                ),
-                
-                Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip2".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip2Tag".fromLocalized()
-                ),
-                
-                Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip3".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip3Tag".fromLocalized()
-                ),
-                
-                Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip4".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip4Tag".fromLocalized()
-                ),
-                
-                Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip5".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip5Tag".fromLocalized()
-                ),
-                
-                Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip6".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip6Tag".fromLocalized()
-                ),
-                
-                Challenge<String>.TaggedString(
-                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip7".fromLocalized(),
-                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.CALCULATED.ProTip7Tag".fromLocalized()
-                ),
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.ProTip1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.ProTip1Tag".fromLocalized()
+                )
             ]
         ),
-
+        
+        
+        Challenge<String>(
+            name: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.ChallengeName".fromLocalized(),
+            map: "the final reich",
+            riddle: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.Riddle".fromLocalized(),
+            requirements: [
+                Challenge<String>.TaggedString(
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.Requirement1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.Requirement1Tag".fromLocalized()
+                )
+            ],
+            donts: [],
+            glitches: [],
+            proTips: [
+                Challenge<String>.TaggedString(
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.ProTip1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.UNSCATHED.ProTip1Tag".fromLocalized()
+                )
+            ]
+        ),
+        
+        
+        Challenge<String>(
+            name: "OUTFIT.B.A.T._ELITE.OFF_RECORD.OLD_SCHOOL.ChallengeName".fromLocalized(),
+            map: "the final reich",
+            riddle: "OUTFIT.B.A.T._ELITE.OFF_RECORD.OLD_SCHOOL.Riddle".fromLocalized(),
+            requirements: [
+                Challenge<String>.TaggedString(
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.OLD_SCHOOL.Requirement1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.OLD_SCHOOL.Requirement1Tag".fromLocalized()
+                )
+            ],
+            donts: [
+                Challenge<String>.TaggedString(
+                    "OUTFIT.B.A.T._ELITE.OFF_RECORD.OLD_SCHOOL.Dont1".fromLocalized(),
+                    tag: "OUTFIT.B.A.T._ELITE.OFF_RECORD.OLD_SCHOOL.Dont1Tag".fromLocalized()
+                )
+            ],
+            glitches: [],
+            proTips: []
+        ),
     ]
 }
