bf52f20be1b450774bc95dfb53f4646a77dd0520
= <niccolo.dellarocca@gmail.com>
2024-10-19 12:46:26 +0200

    Attempted to fix missing tokens

--- Sources/ZTronWWIIUniforms/Model/Challenge, TaggedString.swift
+++ Sources/ZTronWWIIUniforms/Model/Challenge, TaggedString.swift
@@ -94,3 +94,10 @@ public final class Challenge<Tag>: Hashable, Identifiable, Sendable where Tag: H
         }
     }
 }
+
+
+extension Challenge<String>.TaggedString {
+    func getTag() -> String {
+        return self.tag.fromLocalized()
+    }
+}
